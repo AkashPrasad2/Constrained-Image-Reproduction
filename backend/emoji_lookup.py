da19ef500b48d74fdb226a5cc75aaeeb92b71228
Akash Prasad <akashprasad19@gmail.com>
2026-01-03 16:12:40 -0500

    added emoji lookup class

--- backend/emoji_lookup.py
+++ backend/emoji_lookup.py
@@ -1,10 +1,41 @@
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import os
 
 
-def render_image():
-    img = Image.open("./emojis/monkey.png")
-    img.show()
+class EmojiLookup:
+    """Lookup table for emojis"""
+
+    def __init__(self, _emojis_path="./emojis"):
+        self.lookup = {}  # emoji name: [r,b,g averages]
+        self.emojis_path = _emojis_path
+
+        self._build_lookup()
+
+    def _build_lookup(self):
+        for file in os.listdir(self.emojis_path):
+            path = os.path.join(self.emojis_path, file)
+            img = Image.open(path)
+            averages = self._compute_average_rgb(img)
+            self.lookup[file] = averages
+
+    def _compute_average_rgb(self, emoji: Image) -> tuple[float, float, float]:
+        """
+        Compute average rbg value of emoji to store in lookup table
+
+        Args:
+            emoji: PIL image of emoji
+        Returns: 
+            (r_avg, g_avg, b_avg) : tuple of np.float64
+        """
+        emoji = emoji.convert("RGBA")
+        r, g, b, _ = emoji.split()  # split into 3 Images of type L
+        r_arr = np.array(r)
+        g_arr = np.array(g)
+        b_arr = np.array(b)
+
+        return (np.mean(r_arr), np.mean(g_arr), np.mean(b_arr))  # return tuple
 
 
 if __name__ == "__main__":
-    render_image()
+    img = Image.open("./emojis/monkey.png")
